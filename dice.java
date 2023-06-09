1cc25c5821bcfa37d636834b134e9adec3acde4d
OMUROKeiya <136040549+OMUROKeiya@users.noreply.github.com>
2023-06-09 12:13:48 +0900

    Dice game: initial revision

---
+++ dice.java
@@ -0,0 +1,16 @@
+package start.java;
+
+import java.util.Random;
+class dice{
+    public static void main(String args[]){
+        System.out.println("Rolling dice...");
+        Random r = new Random();
+        int num,i,sum = 0;
+        for(i = 1; i <= 2; i++){
+            num = r.nextInt(6) + 1;
+            System.out.println("Die" + i + ": " + num);
+            sum += num;
+        }
+        System.out.println("Total value: " + sum);
+    }
+}
